4e7e2a90c8ab0b4cb21c38f34c2febb669976352
Rhys Malyon <malyonr@gmail.com>
2021-09-29 20:52:43 +0900

    Add files for GitHub pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-weather-app/'
+  : '/',
+  
+
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 
